f6ffa681ef4815ba64d586ef8576d82747b79d3c
Fritz kenne <kennefritzon@gmail.com>
2019-04-24 16:21:42 +0100

    ajout du composant Input

--- src/components/Pages/main.js
+++ src/components/Pages/main.js
@@ -29,12 +29,22 @@ class Main extends Component {
                     <Content style={{ margin: '24px 16px 0', overflow: 'initial',height:'85vh' }}>
                         <div style={{ padding: 24, background: '#fff', textAlign: 'center',height: '98%' }}>
                           <Input
-                            placeholder="Entrez votre nom"
+                            placeholder="rechercher"
                             prefix //when true Input has a prefixed icon
                             prefixIconType="user"
                             suffix //when true Input has a suffixed icon to give a user more informations about him
                             suffixDescription="ce champ represente le nom de l'utilisateur"
                             suffixIconType="info-circle"
+                            addOnBefore
+                            addOnAfter
+                            beforeText={"Http://"}
+                            afterText={".com"}
+                            defaultValue="mon site"
+                            //type="textarea"
+                            searchable
+                            size="large"
+                            enterButtonText="chercher"
+                            onSearch={(value) => alert(value) }
                           />
                         </div>
                     </Content>
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,28 +1,73 @@
 import React, { Component } from 'react';
-import { Input, Tooltip, Icon } from 'antd';
+import { Input, Tooltip, Icon, Select } from 'antd';
+
+const Option = Select.Option;
+const Search = Input.Search;
+const { TextArea } = Input;
+const selectBefore = (
+  <Select defaultValue="Http://" style={{ width: 90 }}>
+    <Option value="Http://">Http://</Option>
+    <Option value="Https://">Https://</Option>
+  </Select>
+);
+const selectAfter = (
+  <Select defaultValue=".com" style={{ width: 80 }}>
+    <Option value=".com">.com</Option>
+    <Option value=".jp">.jp</Option>
+    <Option value=".cn">.cn</Option>
+    <Option value=".org">.org</Option>
+  </Select>
+);
+
 
 class EMCInput extends Component {
-    render() {
-        const { 
-            placeholder,
-            prefix,
-            suffix,
-            prefixIconType,
-            suffixDescription,
-            suffixIconType
-        } = this.props;
-        return (
-            <Input
-                placeholder={placeholder}
-                prefix={prefix ? <Icon type={prefixIconType} style={{ color: 'rgba(0,0,0,.25)' }} /> : ''}
-                suffix={
-                    suffix ?
-                        <Tooltip title={suffixDescription}>
-                            <Icon type={suffixIconType} style={{ color: 'rgba(0,0,0,.45)' }} />
-                        </Tooltip> : ''
-                }
-            />
-        )
+  render() {
+    const {
+      placeholder,
+      prefix,
+      suffix,
+      prefixIconType,
+      suffixDescription,
+      suffixIconType,
+      addOnBefore,
+      beforeText,
+      addOnAfter,
+      afterText, 
+      onSearch,
+      enterButtonText,
+      size,
+      type
+    } = this.props;
+    if (type === "searchable") { 
+      return (
+        <Search
+          placeholder={placeholder}
+          enterButton={enterButtonText ? enterButtonText : ''}
+          size={size}
+          onSearch={value => onSearch(value)}
+          //style={{ width: 200 }}
+        />
+      )
     }
+    if (type === "textarea") {
+      return (
+        <TextArea placeholder="Autosize height based on content lines" autosize />
+      )
+    }
+    return (
+      <Input
+        placeholder={placeholder}
+        prefix={prefix ? <Icon type={prefixIconType} style={{ color: 'rgba(0,0,0,.25)' }} /> : ''}
+        suffix={
+          suffix ?
+            <Tooltip title={suffixDescription}>
+              <Icon type={suffixIconType} style={{ color: 'rgba(0,0,0,.45)' }} />
+            </Tooltip> : ''
+        }
+        addonBefore={addOnBefore ? beforeText : ''}
+        addonAfter={addOnAfter ? afterText : ''}
+      />
+    )
+  }
 }
 export default EMCInput;
